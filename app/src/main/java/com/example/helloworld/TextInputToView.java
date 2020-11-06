d8fc715bc9f1c1e1b2f55c849c46389d63831ac9
YannikKintscher <playeryannik@gmai.com>
2020-11-06 15:49:00 +0100

    Implemented clear all button

--- app/src/main/java/com/example/helloworld/TextInputToView.java
+++ app/src/main/java/com/example/helloworld/TextInputToView.java
@@ -2,6 +2,7 @@ package com.example.helloworld;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -11,18 +12,33 @@ import com.google.android.material.textfield.TextInputEditText;
 
 public class TextInputToView extends AppCompatActivity {
 
+    private static final String text = "Dein Text: ";
+
+    private TextInputEditText textInputField;
+    private TextView textView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_text_input_to_view);
 
-        Button button = findViewById(R.id.button2);
-        button.setOnClickListener(v -> sendTextFromInputToView());
+        Button senden = findViewById(R.id.button2);
+        senden.setOnClickListener(v -> sendTextFromInputToView());
+
+        Button loeschen = findViewById(R.id.button3);
+        loeschen.setOnClickListener(v -> clearAll());
     }
 
+    @SuppressLint("SetTextI18n")
     private void sendTextFromInputToView() {
-        TextInputEditText textInputField = findViewById(R.id.textInputField);
-        TextView textView = findViewById(R.id.textView3);
-        textView.setText(textInputField.getText());
+        textInputField = findViewById(R.id.textInputField);
+        textView = findViewById(R.id.textView3);
+        textView.setText(text + textInputField.getText());
+    }
+
+    @SuppressLint("SetTextI18n")
+    private void clearAll() {
+        textInputField.setText("");
+        textView.setText(text);
     }
 }
